254240a8b32d505dea75b1317d411eb4694d5864
AlexLe024 <112716420+AlexLe024@users.noreply.github.com>
2023-10-29 15:45:28 +0500

    Update task2.py

--- buns/Mod5/task2.py
+++ buns/Mod5/task2.py
@@ -1,58 +1,79 @@
 class Node:
     def __init__(self, data):
         self.data = data
-        self.prev = None
-        self.next = None
+        self.nref = None
+        self.pref = None
 
 class Queue:
     def __init__(self):
-        self.front = None
-        self.rear = None
+        self.start = None
+        self.end = None
 
-    def enqueue(self, value):
-        new_node = Node(value)
-        if self.rear is None:
-            self.front = new_node
-            self.rear = new_node
+    def pop(self):
+        if self.start is None:
+            return None
+        val = self.start.data
+        self.start = self.start.nref
+        if self.start is not None:
+            self.start.pref = None
+        return val
+
+    def push(self, val):
+        new_node = Node(val)
+        if self.start is None:
+            self.start = new_node
+            self.end = new_node
         else:
-            new_node.prev = self.rear
-            self.rear.next = new_node
-            self.rear = new_node
+            new_node.pref = self.end
+            self.end.nref = new_node
+            self.end = new_node
 
-    def dequeue(self):
-        if self.front is None:
-            raise Exception("Очередь пуста, нельзя выполнить dequeue.")
-        value = self.front.data
-        self.front = self.front.next
-        if self.front is None:
-            self.rear = None
-        return value
-
-    def is_empty(self):
-        return self.front is None
+    def insert(self, n, val):
+        new_node = Node(val)
+        current = self.start
+        for _ in range(n - 1):
+            if current is None:
+                return
+            current = current.nref
+        if current is None:
+            return
+        new_node.pref = current
+        new_node.nref = current.nref
+        if current.nref is not None:
+            current.nref.pref = new_node
+        current.nref = new_node
 
     def print(self):
-        current = self.front
-        while current:
+        current = self.start
+        while current is not None:
             print(current.data, end=" ")
-            current = current.next
+            current = current.nref
         print()
 
-queue = Queue()
+q = Queue()
 
 while True:
-    try:
-        value = input("Введите элемент (или 'q' для завершения): ")
-        if value == 'q':
-            break
-        queue.enqueue(value)
-    except Exception as e:
-        print(e)
+    print("1. Добавить элемент")
+    print("2. Извлечь элемент")
+    print("3. Вставить элемент")
+    print("4. Вывести очередь")
+    print("5. Выйти")
+    choice = input("Выберите действие: ")
 
-queue.print()
-
-while not queue.is_empty():
-    value = queue.dequeue()
-    print(f"Извлечено из очереди: {value}")
-
-print(f"Очередь пуста: {queue.is_empty()}")
+    if choice == "1":
+        val = int(input("Введите элемент для добавления: "))
+        q.push(val)
+    elif choice == "2":
+        val = q.pop()
+        if val is not None:
+            print(f"Извлечен элемент: {val}")
+        else:
+            print("Очередь пуста.")
+    elif choice == "3":
+        n = int(input("Введите позицию для вставки: "))
+        val = int(input("Введите элемент для вставки: "))
+        q.insert(n, val)
+    elif choice == "4":
+        q.print()
+    elif choice == "5":
+        break
